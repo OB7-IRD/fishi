e4a3ac4005db7c0864405d5f16ee322faa6377b7
ClaraLerebourg <claraa.lerebourg@gmail.com>
2023-02-13 11:28:58 +0100

    fishing_activity update

--- R/fishing_activity.R
+++ R/fishing_activity.R
@@ -13,11 +13,10 @@
 #' @importFrom graphics axis lines abline legend barplot mtext
 fishing_activity <- function(data_connection,
                              time_period,
-                             country,
-                             vessel_type = as.integer(x = c(1))
-) {
+                             country = as.integer(x = 1),
+                             vessel_type = as.integer(x = 1),
+                             ocean = as.integer(x = 1)) {
   # 0 - Global variables assignement ----
-  ocean <- NULL
   activity_date <- NULL
   v_nb_calees <- NULL
   v_nb_calee_pos <- NULL
@@ -31,35 +30,35 @@ fishing_activity <- function(data_connection,
                                                               "balbaya_fishing_activity.sql",
                                                               package = "fishi")),
                                   collapse = "\n")
-    fishing_activity_sql_final <- DBI::sqlInterpolate(conn = data_connection[[2]],
-                                                      sql  = fishing_activity_sql,
-                                                      time_period = DBI::SQL(paste(time_period,
-                                                                                   collapse = ", ")),
-                                                      country     = DBI::SQL(paste(country,
-                                                                                   collapse = ", ")),
-                                                      vessel_type = DBI::SQL(paste(vessel_type,
-                                                                                   collapse = ", ")),
-                                                      ocean = DBI::SQL(paste(ocean,
-                                                                             collapse = ", ")))
-    fishing_activity_data <- dplyr::tibble(DBI::dbGetQuery(conn      = data_connection[[2]],
-                                                           statement = fishing_activity_sql_final))
   } else {
     stop(format(x = Sys.time(),
                 format = "%Y-%m-%d %H:%M:%S"),
          " - Indicator not developed yet for this \"data_connection\" argument.\n",
          sep = "")
   }
+  fishing_activity_sql_final <- DBI::sqlInterpolate(conn = data_connection[[2]],
+                                                    sql  = fishing_activity_sql,
+                                                    time_period = DBI::SQL(paste(time_period,
+                                                                                 collapse = ", ")),
+                                                    country     = DBI::SQL(paste(country,
+                                                                                 collapse = ", ")),
+                                                    vessel_type = DBI::SQL(paste(vessel_type,
+                                                                                 collapse = ", ")),
+                                                    ocean = DBI::SQL(paste(ocean,
+                                                                           collapse = ", ")))
+  fishing_activity_data <- dplyr::tibble(DBI::dbGetQuery(conn      = data_connection[[2]],
+                                                         statement = fishing_activity_sql_final))
   # 3 - Data design ----
   fishing_activity_t1 <- fishing_activity_data %>%
     dplyr::mutate(year = lubridate::year(x = activity_date))
-  #db a1
+  # db a1 - Add : Number of total, positive, and null sets by ALL
   a1 <- fishing_activity_t1 %>%
     dplyr::group_by(year) %>%
     dplyr::summarise(a_total = sum(v_nb_calees, na.rm = TRUE),
                      a_positive = sum(v_nb_calee_pos, na.rm = TRUE),
                      a_null = sum(v_nb_calees - v_nb_calee_pos, na.rm = TRUE),
                      .groups = "drop")
-  #db a2
+  # db a2 - Add : Number of total, positive, and null sets by FOB
   a2 <- fishing_activity_t1 %>%
     dplyr::filter(c_tban == 1) %>%
     dplyr::group_by(year) %>%
@@ -67,7 +66,7 @@ fishing_activity <- function(data_connection,
                      l_positive = sum(v_nb_calee_pos, na.rm = TRUE),
                      l_null = sum(v_nb_calees - v_nb_calee_pos, na.rm = TRUE),
                      .groups = "drop")
-  #db a3
+  # db a3 - Add : Number of total, positive, and null sets by FSC
   a3 <- fishing_activity_t1 %>%
     dplyr::filter(c_tban == 2 | c_tban == 3) %>%
     dplyr::group_by(year) %>%
@@ -75,19 +74,18 @@ fishing_activity <- function(data_connection,
                      f_positive = sum(v_nb_calee_pos, na.rm = TRUE),
                      f_null = sum(v_nb_calees - v_nb_calee_pos, na.rm = TRUE),
                      .groups = "drop")
-  #join db
-  table_sets <- merge(a1,a2, by = "year")
+  # Merge db by Year
+  table_sets <- merge(a1, a2, by = "year")
   table_sets <- merge(table_sets, a3, by = "year")
-  #% LOG
+  # Add column : % LOG
   table_sets <- table_sets %>%
-    dplyr::mutate("%_log" = l_total/a_total )
-  # 4 - Legend design ----
-  # 5 - Graphic design ----
-  graphics::par(mar=c(5,4,4,4))
-  set <- as.matrix(table_sets[,c(5,
+    dplyr::mutate("%_log" = l_total / a_total * 100)
+  # 4 - Graphic design ----
+  graphics::par(mar = c(5, 4, 4, 4))
+  set <- as.matrix(table_sets[, c(5,
                                  8)])
   fig.sets <- graphics::barplot(t(set),
-                      beside = F,
+                      beside = FALSE,
                       ylab = "Number of sets",
                       ylim = c(0, max(set) * 1.6),
                       cex.axis = 1.3,
@@ -95,9 +93,9 @@ fishing_activity <- function(data_connection,
                       xaxt = "n")
   graphics::axis(1,
        at = fig.sets,
-       tick = T,
+       tick = TRUE,
        labels = table_sets$year,
-       line=.8,
+       line = .8,
        cex.axis = 1.3)
   graphics::legend("topleft",
          legend = c("FOB-associated schools",
@@ -107,7 +105,7 @@ fishing_activity <- function(data_connection,
          bty = "n",
          fill = c("black",
                   "lightgrey"))
-  graphics::par(new=T)
+  graphics::par(new = TRUE)
   plot(fig.sets,
        table_sets$`%_log`,
        type = "b",
@@ -121,7 +119,7 @@ fishing_activity <- function(data_connection,
        ylab = "",
        ylim = c(0,
                 100),
-       yaxs="i")
+       yaxs = "i")
   graphics::abline(h = 50,
          col = "darkgrey",
          lwd = 1.3)
@@ -129,8 +127,8 @@ fishing_activity <- function(data_connection,
        at = seq(0,
                 100,
                 20),
-       tick = T,
-       labels = T,
+       tick = TRUE,
+       labels = TRUE,
        las = 0,
        cex.axis = 1.3,
        cex.lab = 1.3,
--- inst/sql/balbaya_fishing_activity.sql
+++ inst/sql/balbaya_fishing_activity.sql
@@ -5,9 +5,6 @@ select
 		,activite.v_tpec
 		,activite.v_dur_cal
 		,activite.c_tban
-		,activite.c_ocea as ocean_id
-		,activite.c_engin as vessel_type_id
-		,bateau.c_pav_b as country_id
 FROM 
 		public.activite
 		join public.bateau on (activite.c_bat = bateau.c_bat)
