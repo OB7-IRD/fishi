24d0aa4a4583d6c10731771e748edd20b64b273f
ClaraLerebourg <claraa.lerebourg@gmail.com>
2023-02-15 16:46:30 +0100

    start development function map_catch_fad_previous

---
+++ inst/sql/balbaya_map_catch_fad_previous.sql
@@ -0,0 +1,26 @@
+select
+	a.cwp11_act
+	,c.n_act
+	,c.d_act
+	,b.c_bat
+	,c.c_esp
+	,c.v_poids_capt
+	,a.v_nb_calee_pos
+	,a.c_ocea
+	,a.c_tban
+	,b.c_pav_b
+from
+	public.activite a
+	join public.bateau b on (a.c_bat = b.c_bat)
+	join public.ocean o on (a.c_ocea = o.c_ocea)
+	JOIN public.capture c on (a.c_bat = c.c_bat 
+								and a.d_act = c.d_act 
+								and a.n_act = c.n_act)
+	join public.pavillon p on (b.c_pav_b = p.c_pav_b)
+	join public.type_bateau tb on (b.c_typ_b = tb.c_typ_b)
+WHERE
+	EXTRACT(year FROM a.d_act) IN (?time_period)
+	AND o.c_ocea IN (?ocean)
+	AND tb.c_engin IN (?vessel_type)
+	AND b.c_pav_b IN (?country)
+;
