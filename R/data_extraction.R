03fa39e214d8a8e21fb32429f53296c7b923b257
ClaraLerebourg <claraa.lerebourg@gmail.com>
2023-06-01 08:43:56 +0200

    data_extraction : add csv_output

--- R/data_extraction.R
+++ R/data_extraction.R
@@ -3,7 +3,6 @@
 #' @description Extracts a dataset from a database or csv
 #' @param type {\link[base]{character}} expected. dabase or csv.
 #' @param data_connection {\link[base]{list}} expected. Output of the function {\link[furdeb]{postgresql_dbconnection}}, which must be done before using the fishing_effort function.
-#' @param database_name {\link[base]{character}}. Name of the database.
 #' @param sql_name {\link[base]{character}}. Name of the sql file, in fishi.
 #' @param csv_path {\link[base]{character}}. Path of the csv.
 #' @param time_period {\link[base]{integer}} expected. Period identification in year.
@@ -22,14 +21,14 @@
 #' @importFrom codama r_type_checking
 data_extraction <- function(type,
                             data_connection = NULL,
-                            database_name = NULL,
                             sql_name = NULL,
                             csv_path = NULL,
                             time_period = NULL,
                             country = NULL,
                             vessel_type = NULL,
                             vessel_type_select = NULL,
-                            ocean = NULL) {
+                            ocean = NULL,
+                            csv_output = FALSE) {
   if (type == "database") {
     # Choose database
     if (data_connection[[1]] == "balbaya") {
@@ -44,9 +43,9 @@ data_extraction <- function(type,
                               collapse = "\n")
     } else if (data_connection[[1]] == "sardara") {
       extraction_sql <- paste(readLines(con = system.file("sql",
-                                                             sql_name,
-                                                             package = "fishi")),
-                                 collapse = "\n")
+                                                          sql_name,
+                                                          package = "fishi")),
+                              collapse = "\n")
     } else if (data_connection[[1]] == "observe_9a") {
       extraction_sql <- paste(readLines(con = system.file("sql",
                                                           sql_name,
@@ -82,14 +81,15 @@ data_extraction <- function(type,
                                                     country     = DBI::SQL(paste(country,
                                                                                  collapse = ", ")),
                                                     engin = DBI::SQL(paste(vessel_type,
-                                                                                 collapse = ", ")),
+                                                                           collapse = ", ")),
                                                     ocean       = DBI::SQL(paste(ocean,
-                                                                                 collapse = ", ")))
+                                                                                 collapse = ", "))
+        )
       } else if (vessel_type_select == "vessel_type") {
         extraction_sql <- gsub(pattern = "\n\t.*\\(\\?engin\\)",
-                              replacement = "",
-                              x = extraction_sql,
-                              perl = TRUE)
+                               replacement = "",
+                               x = extraction_sql,
+                               perl = TRUE)
         extraction_sql_final <- DBI::sqlInterpolate(conn        = data_connection[[2]],
                                                     sql         = extraction_sql,
                                                     time_period = DBI::SQL(paste(time_period,
@@ -106,14 +106,24 @@ data_extraction <- function(type,
              " - Indicator not developed yet for this \"vessel_type_select\" argument.\n",
              sep = "")
       }
-      }
+    }
     database <- dplyr::tibble(DBI::dbGetQuery(conn      = data_connection[[2]],
                                               statement = extraction_sql_final))
+    if (csv_output == TRUE) {
+      write.csv2(database,
+                 file= 'database_fishi.csv',
+                 row.names = FALSE)
+    }
 
   } else if (type == "csv") {
     extraction_csv <- readr::read_delim(csv_path)
-    extraction_csv$activity_date <- as.Date(extraction_csv$activity_date, format = "%d/%m/%Y")
-    extraction_csv$year <- lubridate::year(extraction_csv$activity_date)
+    if ("activity_date" %in% colnames(extraction_csv)) {
+      extraction_csv$activity_date <- as.Date(extraction_csv$activity_date, format = "%d/%m/%Y")
+      extraction_csv$year <- lubridate::year(extraction_csv$activity_date)
+    }
+    if ("landing_date" %in% colnames(extraction_csv)) {
+      extraction_csv$landing_date <- as.Date(extraction_csv$landing_date, format = "%d/%m/%Y")
+    }
     database <- extraction_csv %>%
       dplyr::filter(year %in% time_period,
                     ocean_id %in% ocean,
