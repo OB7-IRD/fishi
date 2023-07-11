38efe8f733254bdd02ed12caf2ea10008e267a9a
Clara Lerebourg <clara.lerebourg@ird.fr>
2023-07-11 16:34:11 +0200

    Creation of the function trip_effort

---
+++ R/trip_effort.R
@@ -0,0 +1,96 @@
+#' @name trip_effort
+#' @title Effort trip
+#' @description Effort trip
+#' @param dataframe_observe {\link[base]{data.frame}} expected. Dataframe from the Observe database. Csv or output of the function {\link[fishi]{data_extraction}}, which must be done before using the data_availability() function.
+#' @param dataframe_t3 {\link[base]{data.frame}} expected. Dataframe from the T3 database. Csv or output of the function {\link[fishi]{data_extraction}}, which must be done before using the data_availability() function.
+#' @param database {\link[base]{character}} expected. Observe or t3. Observe by default.
+#' @param reported_year  {\link[base]{integer}} expected. Year of the report.
+#' @param ocean {\link[base]{character}} expected. Atlantic or Indian. Atlantic by default.
+#' @param flag {\link[base]{character}} expected. Flag of the country; isocode.
+#' @return The function return ggplot R plot.
+#' @export
+#' @importFrom codama r_type_checking
+#' @importFrom graphics par plot axis lines abline legend text
+#' @importFrom plyr . ddply
+trip_effort <- function(dataframe_observe,
+                        dataframe_t3,
+                        database = "observe",
+                        reported_year,
+                        flag,
+                        ocean = "Atlantic") {
+  # 0 - Global variables assignement ----
+  flag <- NULL
+  vessel_activity_code <- NULL
+  trip_id <- NULL
+  # 1 - Arguments verification ----
+  if (codama::r_type_checking(r_object = database,
+                              type = "character",
+                              output = "logical") != TRUE) {
+    return(codama::r_type_checking(r_object = database,
+                                   type = "character",
+                                   output = "message"))
+  }
+  if (codama::r_type_checking(r_object = reported_year,
+                              type = "integer",
+                              output = "logical") != TRUE) {
+    return(codama::r_type_checking(r_object = reported_year,
+                                   type = "character",
+                                   output = "integer"))
+  }
+  if (codama::r_type_checking(r_object = flag,
+                              type = "character",
+                              output = "logical") != TRUE) {
+    return(codama::r_type_checking(r_object = flag,
+                                   type = "character",
+                                   output = "message"))
+  }
+  if (codama::r_type_checking(r_object = ocean,
+                              type = "character",
+                              output = "logical") != TRUE) {
+    return(codama::r_type_checking(r_object = ocean,
+                                   type = "character",
+                                   output = "message"))
+  }
+  # 2 - Data design ----
+  # Observe data
+  dataframe_observe <- subset(dataframe_observe, flag %in% flag)
+  dataframe_observe$year_dbq <- as.numeric(substr(dataframe_observe$trip_end_date, 1, 4))
+  dataframe_observe$year <- as.numeric(substr(dataframe_observe$observation_date, 1, 4))
+  dataframe_observe$quarter <- ceiling(as.numeric(substr(dataframe_observe$observation_date, 6, 7)) / 3)
+  dataframe_observe$month <- as.numeric(substr(dataframe_observe$observation_date, 6, 7))
+  dataframe_observe$route_id <- paste(dataframe_observe$trip_id,
+                                      dataframe_observe$observation_date,
+                                      sep = "#")
+  dataframe_observe$observation_timestamp <- as.POSIXct(paste(dataframe_observe$observation_date,
+                                                              dataframe_observe$observation_time),
+                                                        format = "%Y-%m-%d %H:%M:%S")
+  dataframe_observe$set_order <- NA
+  for (i in unique(dataframe_observe$route_id)) {
+    dataframe_observe[dataframe_observe$route_id == i,
+                      "set_order"] <- order(dataframe_observe[dataframe_observe$route_id == i, ]$observation_timestamp)
+  }
+  # T3 data
+  dataframe_t3 <- subset(dataframe_t3, flag %in% flag)
+  dataframe_t3 <- subset(dataframe_t3, vessel_activity_code %in% c(0, 1, 2, 14))
+  dataframe_t3$year <- as.numeric(substr(dataframe_t3$date, 1, 4))
+  dataframe_t3$ocean[dataframe_t3$ocean == "Atlantique"] <- "Atlantic"
+  dataframe_t3$ocean[dataframe_t3$ocean == "Indien"] <- "Indian"
+  dataframe_t3$t3_school_type <- NA
+  dataframe_t3$t3_school_type[dataframe_t3$school_type == "BL"] <- "FSC"
+  dataframe_t3$t3_school_type[dataframe_t3$school_type == "BO"] <- "FAD"
+  dataframe_t3$t3_school_type[dataframe_t3$school_type == "IND"] <- "UNK"
+  # 3 - Graphic design ----
+  if (database == "observe") {
+    trip_effort_data <-  plyr::ddply(dataframe_observe,
+                                     plyr::.(ocean, reported_year, flag),
+                                     summarise,
+                                     n_trips = length(unique(trip_id)))
+    as.data.frame(trip_effort_data)
+  } else if (database == "t3") {
+    trip_effort_data <- plyr::ddply(dataframe_t3,
+                                    plyr::.(ocean, year, flag),
+                                    summarise,
+                                    n_trips = length(unique(trip_id)))
+    as.data.frame(trip_effort_data)
+  }
+}
---
+++ inst/sql/observe_ps_sets.sql
@@ -0,0 +1,47 @@
+SELECT
+	o.label1 AS ocean,
+	o.code AS ocean_code,
+	p.label1 AS program,
+	v.label1 AS vessel,
+	v.code AS vessel_code,
+	c.iso3code AS flag,
+	v.fleetcountry AS fleet_country,
+	ob.lastname::text AS observer_name,
+	t.startdate::date AS trip_start_date,
+	t.enddate::date AS trip_end_date,
+	r.date::date AS observation_date,
+	a.time AS observation_time,
+	va.label1 AS vessel_activity,
+	va.code AS vessel_activity_code,
+	a.latitude AS latitude,
+	a.longitude AS longitude,
+	(CASE WHEN st.code='0' THEN 'UNK' ELSE (CASE WHEN st.code='1' THEN 'FOB' ELSE (CASE WHEN st.code='2' THEN 'FSC' END) END) END) AS school_type,
+	t.topiaid AS trip_id,
+	s.topiaid AS set_id
+
+FROM ps_common.trip t
+	INNER JOIN ps_common.program p ON (t.observationsprogram = p.topiaid)
+	INNER JOIN common.ocean o ON (t.ocean = o.topiaid)
+	INNER JOIN common.person ob ON (t.observer = ob.topiaid)
+	INNER JOIN common.vessel v ON (t.vessel = v.topiaid)
+	INNER JOIN common.vesseltype vt ON (v.vesseltype = vt.topiaid)
+	INNER JOIN common.country c ON (v.flagcountry = c.topiaid)
+	INNER JOIN ps_observation.route r ON (r.trip = t.topiaid)
+	INNER JOIN ps_observation.activity a ON (a.route = r.topiaid)
+	INNER JOIN ps_common.vesselactivity va ON (a.vesselactivity = va.topiaid)
+	LEFT OUTER JOIN ps_observation.set s ON (s.activity = a.topiaid)
+	LEFT OUTER JOIN ps_common.schooltype st ON (s.schooltype = st.topiaid)
+	LEFT OUTER JOIN ps_common.reasonfornullset rns ON (s.reasonfornullset = rns.topiaid)
+	LEFT OUTER JOIN common.fpazone fpa ON (a.currentfpazone = fpa.topiaid)
+
+WHERE
+	extract(year from r.date) IN (?time_period)
+	AND c.code::numeric  IN (?country)
+	AND vt.code::numeric IN (?vessel_type)
+	AND o.code::numeric IN (?ocean)
+	AND p.topiaid IN ('fr.ird.referential.ps.common.Program#1239832686262#0.31033946454061234', -- DCF (IRD)
+					  'fr.ird.referential.ps.common.Program#1308048349668#0.7314513252652438', -- DCF (TAAF)
+				  	  'fr.ird.referential.ps.common.Program#1363095174385#0.011966550987014823', -- Moratoire ICCAT 2013-2015 (IRD)
+				  	  'fr.ird.referential.ps.common.Program#1373642516190#0.998459307142491') -- OCUP
+	AND va.code LIKE '6' 
+;
