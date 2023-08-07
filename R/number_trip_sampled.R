7686c4840fd9e2302118d9abc0dda95dcba12ec0
Clara Lerebourg <clara.lerebourg@ird.fr>
2023-08-07 16:26:54 +0200

    number_trip_sampled : add a connection to the observe database

--- R/number_trip_sampled.R
+++ R/number_trip_sampled.R
@@ -2,10 +2,10 @@
 #' @title trip sampled
 #' @description Give the number of trip sampled for a given year.
 #' @param dataframe {\link[base]{data.frame}} expected. Csv or output of the function {\link[fishi]{data_extraction}}, which must be done before using the fishing_activity() function.
+#' @param data_type {\link[base]{character}} expected. Tunabio or observe.
 #' @param graph_type {\link[base]{character}} expected. "number" or "table." Number by default.
-#' @param reported_year {\link[base]{integer}} expected. Write the wanted year of the report
-#' @param selected_country {\link[base]{integer}} expected. Country code to select the list of boat to count. If NULL give all the vessel for the given year.
-#' @param title TRUE or FALSE expected. False by default.
+#' @param reported_year {\link[base]{integer}} expected. Year of the report, for tunabio only.
+#' @param country {\link[base]{integer}} expected. Country code to select the list of boat to count. If NULL give all the vessel for the given year.
 #' @return The function return ggplot or table R plot.
 #' @export
 #' @importFrom readxl read_excel
@@ -17,10 +17,10 @@
 #' @importFrom tidyr pivot_longer
 #' @importFrom codama r_type_checking
 number_trip_sampled <- function(dataframe,
-                                  graph_type = "number",
-                                  reported_year = NULL,
-                                  selected_country = NULL,
-                                  title = FALSE) {
+                                data_type,
+                                graph_type = "number",
+                                reported_year = NULL,
+                                country = NULL) {
   # 0 - Global variables assignement ----
   fish_sampling_date <- NULL
   landing_date <- NULL
@@ -31,6 +31,8 @@ number_trip_sampled <- function(dataframe,
   nb_vessel <- NULL
   country <- NULL
   sampled_trip_summarize <- NULL
+  boat_code <- NULL
+  fleet <- NULL
   STATUT <- NULL
   NOMBAT <- NULL
   NUMBAT <- NULL
@@ -45,127 +47,149 @@ number_trip_sampled <- function(dataframe,
                                    output = "message"))
   }
   # 2 - Data design ----
-  ## Data import -----
-  tunabio <- vector("list")
-  tunabio[["biology"]] <- readxl::read_excel(path = dataframe,
-                                             sheet = "SPECIMEN",
-                                             col_types = c("text",
-                                                           "text",
-                                                           "text",
-                                                           "text",
-                                                           "date",
-                                                           "text",
-                                                           "text",
-                                                           "text",
-                                                           "text",
-                                                           "text",
-                                                           "numeric",
-                                                           "numeric",
-                                                           "numeric",
-                                                           "numeric",
-                                                           "text",
-                                                           "numeric",
-                                                           "numeric",
-                                                           "numeric",
-                                                           "numeric",
-                                                           "text",
-                                                           "numeric",
-                                                           "numeric",
-                                                           "numeric",
-                                                           "numeric",
-                                                           "text",
-                                                           "numeric",
-                                                           "numeric",
-                                                           "numeric",
-                                                           "numeric",
-                                                           "numeric",
-                                                           "numeric",
-                                                           "text",
-                                                           "text",
-                                                           "text",
-                                                           "numeric",
-                                                           "numeric",
-                                                           "numeric",
-                                                           "numeric",
-                                                           "numeric",
-                                                           "numeric",
-                                                           "numeric",
-                                                           "text",
-                                                           "numeric",
-                                                           "text",
-                                                           "text",
-                                                           "text",
-                                                           "text"),
-                                             na = "na")
+  if (data_type == "tunabio") {
+    ## Data import -----
+    tunabio <- vector("list")
+    tunabio[["biology"]] <- readxl::read_excel(path = dataframe,
+                                               sheet = "SPECIMEN",
+                                               col_types = c("text",
+                                                             "text",
+                                                             "text",
+                                                             "text",
+                                                             "date",
+                                                             "text",
+                                                             "text",
+                                                             "text",
+                                                             "text",
+                                                             "text",
+                                                             "numeric",
+                                                             "numeric",
+                                                             "numeric",
+                                                             "numeric",
+                                                             "text",
+                                                             "numeric",
+                                                             "numeric",
+                                                             "numeric",
+                                                             "numeric",
+                                                             "text",
+                                                             "numeric",
+                                                             "numeric",
+                                                             "numeric",
+                                                             "numeric",
+                                                             "text",
+                                                             "numeric",
+                                                             "numeric",
+                                                             "numeric",
+                                                             "numeric",
+                                                             "numeric",
+                                                             "numeric",
+                                                             "text",
+                                                             "text",
+                                                             "text",
+                                                             "numeric",
+                                                             "numeric",
+                                                             "numeric",
+                                                             "numeric",
+                                                             "numeric",
+                                                             "numeric",
+                                                             "numeric",
+                                                             "text",
+                                                             "numeric",
+                                                             "text",
+                                                             "text",
+                                                             "text",
+                                                             "text"),
+                                               na = "na")
 
-  tunabio[["env"]] <- readxl::read_excel(path = dataframe,
-                                         sheet = "ENVIRONMENT",
-                                         col_types = c("text",
-                                                       "text",
-                                                       "text",
-                                                       "text",
-                                                       "text",
-                                                       "text",
-                                                       "text",
-                                                       "text",
-                                                       "text",
-                                                       "text",
-                                                       "text",
-                                                       "date",
-                                                       "date",
-                                                       "date",
-                                                       "date",
-                                                       "numeric",
-                                                       "text",
-                                                       "numeric",
-                                                       "text",
-                                                       "text",
-                                                       "text",
-                                                       "text",
-                                                       "text",
-                                                       "text",
-                                                       "text"),
-                                         na = "na")
-  tunabio[["vessel"]] <- readxl::read_excel("~/Echantillonnage biologique/OI/Tunabio/Tunabio OI 20230619.xlsx",
-                                            sheet = "vessel") %>%
-    dplyr::filter(STATUT == 1) %>%
-    dplyr::select(vessel_name = NOMBAT, boat_code = NUMBAT, country = PAYS, fleet = FLOTTE)
-  ## Data manipulation ----
-  tunabio[["biology"]] <- dplyr::mutate(.data =  tunabio[["biology"]],
-                                        sampling_year = lubridate::year(fish_sampling_date),
-                                        fish_sampling_date = lubridate::date(fish_sampling_date))
-  ## mean fishing date
-  tunabio[["env"]] <- dplyr::mutate(.data = tunabio[["env"]],
-                                    landing_date = lubridate::date(landing_date))
-  ## merge of ENV and BIO data
-  tunabio[["merged"]] <- dplyr::left_join(x = tunabio[["biology"]],
-                                          y = tunabio[["env"]],
-                                          by = "fish_identifier",
-                                          relationship = "many-to-many") %>%
-    dplyr::select(fish_identifier,
-                  fish_sampling_date,
-                  sampling_year,
-                  vessel_code,
-                  vessel_name,
-                  landing_date)
-  ## Data analyze ----
-  if (!is.null(selected_country)) {
-    sampled_trip_summarize <-  tunabio[["merged"]] %>%
-      dplyr::filter(sampling_year == reported_year) %>%
-      dplyr::group_by(vessel_name, landing_date) %>%
-      dplyr::summarise(nb_vessel = dplyr::n_distinct(vessel_name), .groups = "drop") %>%
-      dplyr::left_join(y = tunabio[["vessel"]], by = dplyr::join_by(vessel_name)) %>%
-      dplyr::select(-nb_vessel) %>%
-      dplyr::filter(country == selected_country)
-  } else if (is.null(selected_country)) {
-    sampled_trip_summarize <-  tunabio[["merged"]] %>%
-      dplyr::filter(sampling_year == reported_year) %>%
-      dplyr::group_by(vessel_name, landing_date) %>%
-      dplyr::summarise(nb_trip = dplyr::n_distinct(vessel_name), .groups = "drop") %>%
-      dplyr::left_join(y = tunabio[["vessel"]], by = dplyr::join_by(vessel_name)) %>%
-      dplyr::select(-nb_vessel) %>%
+    tunabio[["env"]] <- readxl::read_excel(path = dataframe,
+                                           sheet = "ENVIRONMENT",
+                                           col_types = c("text",
+                                                         "text",
+                                                         "text",
+                                                         "text",
+                                                         "text",
+                                                         "text",
+                                                         "text",
+                                                         "text",
+                                                         "text",
+                                                         "text",
+                                                         "text",
+                                                         "date",
+                                                         "date",
+                                                         "date",
+                                                         "date",
+                                                         "numeric",
+                                                         "text",
+                                                         "numeric",
+                                                         "text",
+                                                         "text",
+                                                         "text",
+                                                         "text",
+                                                         "text",
+                                                         "text",
+                                                         "text"),
+                                           na = "na")
+    tunabio[["vessel"]] <- readxl::read_excel(path = dataframe,
+                                              sheet = "vessel") %>%
+      dplyr::filter(STATUT == 1) %>%
+      dplyr::select(vessel_name = NOMBAT,
+                    boat_code = NUMBAT,
+                    country = PAYS,
+                    fleet = FLOTTE)
+    ## Data manipulation ----
+    tunabio[["biology"]] <- dplyr::mutate(.data =  tunabio[["biology"]],
+                                          sampling_year = lubridate::year(fish_sampling_date),
+                                          fish_sampling_date = lubridate::date(fish_sampling_date))
+    ## mean fishing date
+    tunabio[["env"]] <- dplyr::mutate(.data = tunabio[["env"]],
+                                      landing_date = lubridate::date(landing_date))
+    ## merge of ENV and BIO data
+    tunabio[["merged"]] <- dplyr::left_join(x = tunabio[["biology"]],
+                                            y = tunabio[["env"]],
+                                            by = "fish_identifier",
+                                            relationship = "many-to-many") %>%
+      dplyr::select(fish_identifier,
+                    fish_sampling_date,
+                    sampling_year,
+                    vessel_code,
+                    vessel_name,
+                    landing_date)
+    ## Data analyze ----
+    if (!is.null(country)) {
+      sampled_trip_summarize <-  tunabio[["merged"]] %>%
+        dplyr::filter(sampling_year == reported_year) %>%
+        dplyr::group_by(vessel_name,
+                        landing_date) %>%
+        dplyr::summarise(nb_vessel = dplyr::n_distinct(vessel_name),
+                         .groups = "drop") %>%
+        dplyr::left_join(y = tunabio[["vessel"]],
+                         by = dplyr::join_by(vessel_name)) %>%
+        dplyr::select(-nb_vessel) %>%
+        dplyr::filter(country == country)
+    } else if (is.null(country)) {
+      sampled_trip_summarize <-  tunabio[["merged"]] %>%
+        dplyr::filter(sampling_year == reported_year) %>%
+        dplyr::group_by(vessel_name,
+                        landing_date) %>%
+        dplyr::summarise(nb_trip = dplyr::n_distinct(vessel_name),
+                         .groups = "drop") %>%
+        dplyr::left_join(y = tunabio[["vessel"]],
+                         by = dplyr::join_by(vessel_name)) %>%
+        dplyr::select(-nb_vessel) %>%
+        dplyr::filter(!is.na(vessel_name))
+    }
+  } else if (data_type == "observe") {
+    sampled_trip_summarize <- dataframe %>%
+      dplyr::group_by(vessel_name,
+                      landing_date,
+                      boat_code,
+                      country,
+                      fleet) %>%
+      dplyr::summarise(nb_trip = dplyr::n_distinct(vessel_name),
+                       .groups = "drop")  %>%
       dplyr::filter(!is.na(vessel_name))
   }
+
   # 3 - Legend design ----
   # 4 - Graphic design ----
   if (graph_type == "number") {
---
+++ inst/sql/observe_nb_trip.sql
@@ -0,0 +1,24 @@
+SELECT
+	EXTRACT(YEAR FROM r.date) as sampling_year
+	,r.date as fish_sampling_date
+	,t.enddate as landing_date
+	,v.label1 as vessel_name
+	,v.code::numeric AS boat_code
+	,ct.iso3code AS fleet
+	,o.code::numeric as ocean_id
+	,ct.code::numeric as country
+FROM ps_logbook.catch c
+	INNER JOIN ps_logbook.activity a ON c.activity = a.topiaid 
+	INNER JOIN ps_logbook.route r ON a.route = r.topiaid
+	INNER JOIN ps_common.trip t ON r.trip = t.topiaid
+	INNER JOIN common.ocean o ON t.ocean = o.topiaid
+	INNER JOIN common.vessel v ON t.vessel = v.topiaid
+	INNER JOIN common.country ct ON v.flagcountry = ct.topiaid 
+	INNER JOIN common.vesseltype vt ON (v.vesseltype = vt.topiaid)
+	INNER JOIN common.species s ON (c.species = s.topiaid)
+WHERE
+	EXTRACT(year FROM r.date) IN (?time_period)
+	AND ct.code::numeric  IN (?country)
+	AND vt.code::numeric IN (?vessel_type)
+	AND o.code::numeric IN (?ocean)
+;
