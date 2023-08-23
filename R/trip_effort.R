e74565621043e071ea7be153862ac310694edc40
Clara Lerebourg <clara.lerebourg@ird.fr>
2023-08-23 11:22:25 +0200

    flag_selected update

--- R/trip_effort.R
+++ R/trip_effort.R
@@ -6,7 +6,7 @@
 #' @param database {\link[base]{character}} expected. Observe or t3. Observe by default.
 #' @param reported_year  {\link[base]{integer}} expected. Year of the report.
 #' @param ocean {\link[base]{character}} expected. Atlantic or Indian. Atlantic by default.
-#' @param flag {\link[base]{character}} expected. Flag of the country; isocode.
+#' @param flag_selected {\link[base]{character}} expected. Flag of the country; isocode.
 #' @return The function return ggplot R plot.
 #' @export
 #' @importFrom codama r_type_checking
@@ -16,7 +16,7 @@ trip_effort <- function(dataframe_observe,
                         dataframe_t3,
                         database = "observe",
                         reported_year,
-                        flag,
+                        flag_selected,
                         ocean = "Atlantic") {
   # 0 - Global variables assignement ----
   flag <- NULL
@@ -52,7 +52,7 @@ trip_effort <- function(dataframe_observe,
   # 2 - Data design ----
   # Observe data
   dataframe_observe <- dataframe_observe %>%
-    dplyr::filter(flag %in% flag) %>%
+    dplyr::filter(flag_selected == flag) %>%
     dplyr::mutate(year_dbq = as.numeric(substr(trip_end_date, 1, 4)),
                   year = as.numeric(substr(observation_date, 1, 4)),
                   quarter = ceiling(as.numeric(substr(observation_date, 6, 7)) / 3),
@@ -67,7 +67,7 @@ trip_effort <- function(dataframe_observe,
     dplyr::mutate(set_order = order(observation_timestamp))
   # T3 data
   dataframe_t3 <- dataframe_t3 %>%
-    dplyr::filter(flag %in% flag) %>%
+    dplyr::filter(flag_selected == flag) %>%
     dplyr::filter(vessel_activity_code %in% c(0, 1, 2, 14)) %>%
     dplyr::mutate(year = as.numeric(substr(date, 1, 4)),
                   ocean = dplyr::case_when(ocean == "Atlantique" ~ "Atlantic",
@@ -80,12 +80,16 @@ trip_effort <- function(dataframe_observe,
   # 3 - Graphic design ----
   if (database == "observe") {
     trip_effort_data <-  dataframe_observe %>%
-      dplyr::group_by(ocean, year, flag) %>%
+      dplyr::group_by(ocean,
+                      year,
+                      flag) %>%
       dplyr::summarise(n_trips = dplyr::n_distinct(trip_id))
     as.data.frame(trip_effort_data)
   } else if (database == "t3") {
     trip_effort_data <- dataframe_t3 %>%
-      dplyr::group_by(ocean, year, flag) %>%
+      dplyr::group_by(ocean,
+                      year,
+                      flag) %>%
       dplyr::summarise(n_trips = dplyr::n_distinct(trip_id))
     as.data.frame(trip_effort_data)
   }
