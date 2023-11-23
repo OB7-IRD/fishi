45988586d8f1baa5f2a947e918ddf7f8d5317283
Clara Lerebourg <clara.lerebourg@ird.fr>
2023-11-23 08:51:24 +0100

    update plot and save as png

--- R/data_availability.R
+++ R/data_availability.R
@@ -4,9 +4,9 @@
 #' @param dataframe_observe {\link[base]{data.frame}} expected. Dataframe from the Observe database. Csv or output of the function {\link[fishi]{data_extraction}}, which must be done before using the data_availability() function.
 #' @param dataframe_t3 {\link[base]{data.frame}} expected. Dataframe from the T3 database. Csv or output of the function {\link[fishi]{data_extraction}}, which must be done before using the data_availability() function.
 #' @param dataframe_vms {\link[base]{data.frame}} expected. Dataframe from the Vms database. Csv or output of the function {\link[fishi]{data_extraction}}, which must be done before using the data_availability() function.
-#' @param graph_type {\link[base]{character}} expected. plot or table. Plot by default.
 #' @param reported_year  {\link[base]{integer}} expected. Year of the report.
 #' @param ocean {\link[base]{character}} expected. Atlantic or Indian Atlantic by default.
+#' @param path_to_png {\link[base]{character}} expected. Add a link to the path to save the figure as a png.
 #' @return The function return ggplot R plot.
 #' @export
 #' @importFrom codama r_type_checking
@@ -15,21 +15,14 @@
 data_availability <- function(dataframe_observe,
                               dataframe_t3,
                               dataframe_vms,
-                              graph_type = "ggplot",
                               reported_year,
-                              ocean = "Atlantic") {
+                              ocean = "Atlantic",
+                              path_to_png = NULL) {
   # 0 - Global variables assignement ----
   vesselname <- NULL
   id <- NULL
   observation_date <- NULL
   # 1 - Arguments verification ----
-  if (codama::r_type_checking(r_object = graph_type,
-                              type = "character",
-                              output = "logical") != TRUE) {
-    return(codama::r_type_checking(r_object = graph_type,
-                                   type = "character",
-                                   output = "message"))
-  }
   if (codama::r_type_checking(r_object = reported_year,
                               type = "integer",
                               output = "logical") != TRUE) {
@@ -63,21 +56,20 @@ data_availability <- function(dataframe_observe,
     dataframe_vms <- dataframe_vms[dataframe_vms$longitude > 25, ]
   }
   # 3 - Graphic design ----
-  if (graph_type == "plot") {
-    # Creating a data frame for boat names
-    vessel_data <- data.frame(vessel = as.character(sort(unique(dataframe_t3$vessel))))
-    # Creating a data frame for dates
-    day_data <- data.frame(day = seq(as.Date(paste(min(reported_year), "01-01", sep = "-")),
-                                     as.Date(paste(max(reported_year) + 1, "01-01", sep = "-")),
-                                     by = 1))
-    # Merge the data to create the ggplot graph
-    data <- merge(vessel_data, day_data, all = TRUE)
-    dataframe_vms$date <- as.Date(dataframe_vms$date)
-    dataframe_vms$vesselname <- trimws(dataframe_vms$vesselname)
-    dataframe_vms <- dataframe_vms %>%
-      dplyr::filter(vesselname %in% vessel)
-    # Ggplot
-    (graph <- ggplot2::ggplot(data,
+  # Creating a data frame for boat names
+  vessel_data <- data.frame(vessel = as.character(sort(unique(dataframe_t3$vessel))))
+  # Creating a data frame for dates
+  day_data <- data.frame(day = seq(as.Date(paste(min(reported_year), "01-01", sep = "-")),
+                                   as.Date(paste(max(reported_year) + 1, "01-01", sep = "-")),
+                                   by = 1))
+  # Merge the data to create the ggplot graph
+  data <- merge(vessel_data, day_data, all = TRUE)
+  dataframe_vms$date <- as.Date(dataframe_vms$date)
+  dataframe_vms$vesselname <- trimws(dataframe_vms$vesselname)
+  dataframe_vms <- dataframe_vms %>%
+    dplyr::filter(vesselname %in% vessel)
+  # Ggplot
+  graph <- ggplot2::ggplot(data,
                             ggplot2::aes(x = day,
                                          y = vessel)) +
       ggplot2::geom_blank() +
@@ -97,7 +89,8 @@ data_availability <- function(dataframe_observe,
                              ocean,
                              "Ocean in",
                              reported_year)) +
-      ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5)) +
+      ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5),
+                     plot.margin = ggplot2::margin(6.1, 10.1, 4.1, 2.1)) +
       ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90,
                                                          hjust = 1),
                      panel.background = ggplot2::element_rect(fill = NA)) +
@@ -128,12 +121,19 @@ data_availability <- function(dataframe_observe,
                                              "logbook" = "red",
                                              "observe" = "blue")) +
       ggplot2::theme(legend.position = "bottom") +
-      ggplot2::labs(color = ""))
-    return(graph)
-  } else if (graph_type == "table") {
-    data_availability <-  dataframe_vms %>%
-      dplyr::group_by(vesselname) %>%
-      dplyr::summarise(n = dplyr::n_distinct(id))
-    as.data.frame(data_availability)
+      ggplot2::labs(color = "")
+  if (!is.null(path_to_png)) {
+    ggplot2::ggsave(filename = paste0(path_to_png,
+                                     "availability_observe_vms_ps_fr_",
+                                     tolower(substr(ocean,1,3)),
+                                     "_",
+                                     reported_year,
+                                     ".png"),
+                    plot = graph,
+                    width = 15,
+                    height = 8,
+                    bg = "white",
+                    device='png')
   }
+  return(graph)
 }
