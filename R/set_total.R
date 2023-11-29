b56bf8bd2b8cc82192d621c8554d4c1b062c5a1d
Clara Lerebourg <clara.lerebourg@ird.fr>
2023-11-29 14:20:13 +0100

    Create set_total function

---
+++ R/set_total.R
@@ -0,0 +1,121 @@
+#' @name set_total
+#' @title Total set
+#' @description Total sets events, sampled or not, with or without tuna landings.
+#' @param dataframe {\link[base]{data.frame}} expected. Csv or output of the function {\link[fishi]{data_extraction}}, which must be done before using the fishing_activity() function.
+#' @param graph_type {\link[base]{character}} expected. "number" or "table." Number by default.
+#' @param reported_year {\link[base]{integer}} expected. Write the wanted year of the report.
+#' @param selected_country {\link[base]{integer}} expected. Country code to select the list of boat to count. If NULL give all the vessel for the given year.
+#' @param selected_ocean {\link[base]{integer}} expected. Ocean code to select the list of boat to count. If NULL give all the vessel for the given year, works only for 'data_type' == 'observe'
+#' @param selected_harbour {\link[base]{integer}} expected. Harbour code to select the list of boat to count. If NULL give all the vessel for the given year, works only for 'data_type' == 'observe'
+#' @return The function return a table.
+#' @export
+#' @importFrom dplyr mutate filter select group_by summarise left_join join_by n_distinct
+#' @importFrom lubridate year
+#' @importFrom tidyr separate_longer_delim separate
+#' @importFrom codama r_type_checking %>%
+set_total <- function(dataframe,
+                      graph_type = "table",
+                      reported_year = NULL,
+                      selected_country = NULL,
+                      selected_ocean = NULL,
+                      selected_harbour = NULL) {
+  # 0 - Global variables assignement ----
+  fleet <- NULL
+  landing_date <- NULL
+  vessel_name <- NULL
+  country_id <- NULL
+  ocean_id <- NULL
+  harbour_id <- NULL
+  landing_year <- NULL
+  program <- NULL
+  ocean_name <- NULL
+  departure <- NULL
+  port_departure <- NULL
+  arrival <- NULL
+  port_arrival <- NULL
+  vessel_type <- NULL
+  # 1 - Arguments verification ----
+  # graph type
+  if (codama::r_type_checking(r_object = graph_type,
+                              type = "character",
+                              output = "logical") != TRUE) {
+    return(codama::r_type_checking(r_object = graph_type,
+                                   type = "character",
+                                   output = "message"))
+  }
+  # reported year
+  if ((! is.null(x = reported_year))
+      && codama::r_type_checking(r_object = reported_year,
+                                 type = "integer",
+                                 output = "logical") != TRUE) {
+    return(codama::r_type_checking(r_object = reported_year,
+                                   type = "integer",
+                                   output = "message"))
+  }
+  # selected country
+  if ((! is.null(x = selected_country))
+      && codama::r_type_checking(r_object = selected_country,
+                                 type = "integer",
+                                 output = "logical") != TRUE) {
+    return(codama::r_type_checking(r_object = selected_country,
+                                   type = "integer",
+                                   output = "message"))
+  }
+  # selected ocean
+  if ((! is.null(x = selected_ocean))
+      && codama::r_type_checking(r_object = selected_ocean,
+                                 type = "integer",
+                                 output = "logical") != TRUE) {
+    return(codama::r_type_checking(r_object = selected_ocean,
+                                   type = "integer",
+                                   output = "message"))
+  }
+  # 2 - Data design ----
+  # If is null
+  # selected ocean
+  if (is.null(selected_ocean)) {
+    selected_ocean <- as.integer(1:6)
+  }
+  # selected harbour
+  if (is.null(selected_harbour)) {
+    selected_harbour <- as.integer(1:999)
+  }
+  # selected country
+  if (is.null(selected_country)) {
+    selected_country <- as.integer(1:87)
+  }
+  # dataframe filter
+  dataframe <- dataframe %>%
+    dplyr::mutate(landing_year = lubridate::year(x = landing_date))
+  dataframe <- dataframe %>%
+    dplyr::filter(country_id %in% selected_country,
+                  ocean_id %in% selected_ocean,
+                  harbour_id %in% selected_harbour,
+                  landing_year %in% reported_year)
+  (set_summarize <- dataframe %>%
+      dplyr::group_by(program,
+                      ocean_name,
+                      fleet,
+                      vessel_type,
+                      vessel_name,
+                      landing_year,
+                      departure,
+                      port_departure,
+                      arrival,
+                      port_arrival) %>%
+      dplyr::summarise(.groups = "drop")  %>%
+      dplyr::group_by(landing_year,
+                      ocean_name,
+                      port_arrival,
+                      fleet,
+                      vessel_type,
+                      vessel_name) %>%
+      dplyr::summarise(nb_trip = dplyr::n_distinct(arrival),
+                       .groups = "drop"))
+  # 3 - Graphic design ----
+  if (graph_type == "number") {
+    sum(set_summarize$nb_trip)
+  } else if (graph_type == "table") {
+    as.data.frame(set_summarize)
+  }
+}
