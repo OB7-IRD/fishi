795d2ddb12ceef99e5db1a40bb526b378bd22c5a
Clara Lerebourg <clara.lerebourg@ird.fr>
2023-12-08 15:26:28 +0100

    standardise spatial occupancy

--- inst/sql/balbaya_spatial_occupancy.sql
+++ inst/sql/balbaya_spatial_occupancy.sql
@@ -1,19 +1,18 @@
 SELECT
 	a.d_act::date AS activity_date
 	,a.cwp11_act
-	,a.v_nb_calees
-	,a.v_nb_calee_pos 
-	,a.v_tpec
-	,a.c_ocea::numeric as ocean_id
-	,b.c_pav_b::numeric as country_id 
-	,a.c_engin::numeric as vessel_type_id
+	,a.v_nb_calees AS total_set
+	,a.v_nb_calee_pos AS positive_set
+	,a.v_tpec AS total_hour_fished
+	,a.c_ocea::numeric AS ocean_code
+	,b.c_pav_b::numeric AS country_code
+	,a.c_engin::numeric AS vessel_type_code
 FROM
 	public.activite a
-	JOIN public.bateau b on (a.c_bat = b.c_bat)
+	JOIN public.bateau b ON (a.c_bat = b.c_bat)
 WHERE
 	EXTRACT(year FROM a.d_act) IN (?time_period)
 	AND b.c_pav_b  IN (?country)
-	AND a.c_engin IN (?engin)
 	AND b.c_typ_b IN (?vessel_type)
 	AND a.c_ocea IN (?ocean)
 ;
