d4ffbf5ba434e72c5b37e06ced63b8b6aaef5e69
Clara Lerebourg <clara.lerebourg@ird.fr>
2024-01-18 14:54:24 +0100

    update sql
    Join sql scripts

--- inst/sql/balbaya_catch_effort.sql
+++ inst/sql/balbaya_catch_effort.sql
@@ -1,8 +1,15 @@
 SELECT
-	a.d_act::date AS activity_date
-	,a.cwp11_act 
+	a.cwp11_act
+	,c.n_act AS activity_id
+	,c.d_act::date AS activity_date
+	,a.v_nb_calee_pos AS positive_set
+	,a.v_nb_calees AS total_set
 	,a.v_tpec AS total_hour_fished
 	,a.v_dur_cal AS set_duration
+	,c.v_poids_capt AS total_catch_weight
+	,b.c_bat AS vessel_code
+	,c.c_esp AS species_code
+	,a.c_tban AS school_code
 	,o.c_ocea::numeric AS ocean_code
 	,e.c_engin::numeric AS vessel_type_code
 	,b.c_pav_b::numeric AS country_code
@@ -12,6 +19,8 @@ FROM
 	JOIN public.capture c ON (a.c_bat = c.c_bat AND a.d_act = c.d_act AND a.n_act = c.n_act)
 	JOIN public.engin e ON (a.c_engin = e.c_engin)
 	JOIN public.ocean o ON (a.c_ocea  = o.c_ocea)
+	JOIN public.pavillon p ON (b.c_pav_b = p.c_pav_b)
+	JOIN public.type_bateau tb ON (b.c_typ_b = tb.c_typ_b)
 WHERE
 	EXTRACT(year FROM a.d_act) IN (?time_period)
 	AND b.c_pav_b  IN (?country)
