acc2aea7a5e70c0ee2dbd2771c4a872fe236e9f9
ClaraLerebourg <claraa.lerebourg@gmail.com>
2024-07-31 11:07:56 +0200

    Add new function density_dcp_1eg

--- R/density_dcp_1deg.R
+++ R/density_dcp_1deg.R
@@ -1,19 +1,19 @@
-#' @name deploiement_density_dcp
-#' @title Map deployment of dcp
-#' @description Map of the the number of deployments of dFADs
-#' @param dataframe {\link[base]{data.frame}} expected. 'Csv' or 'output' of the function {\link[furdeb]{data_extraction}}, which must be done before using the deploiement_density_dcp() function.
+#' @name density_dcp_1deg
+#' @title Density of dcp 1 deg
+#' @description Map of the the density of deployments of dFADs 1 deg (Only for OA for the moment)
+#' @param dataframe {\link[base]{data.frame}} expected. 'Csv' or 'output' of the function {\link[furdeb]{data_extraction}}, which must be done before using the density_dcp_1deg() function.
 #' @details
 #' The input dataframe must contain all these columns for the function to work [\href{https://ob7-ird.github.io/fishi/articles/Db_and_csv.html}{see referentials}]:
 #' \preformatted{
-#'    poly_geom                   | count | year | ocean_code
-#'    --------------------------------------------------------
-#'    ((0 0, 0 1, 1 1, 1 0, 0 0)) |  9    | 2016 | 1
-#'    ((0 0, 0 1, 1 1, 1 0, 0 0)) | 10    | 2014 | 1
-#'    ((0 0, 0 1, 1 1, 1 0, 0 0)) | 11    | 2014 | 1
+#'    count | center_geom_x | center_geom_y |  year  | poly_geom
+#'    ------------------------------------------------------------------------
+#'     110  | 0.5           | 0.5           | 2013 | POLYGON ((0 0, 0 1, 1 1, 1 ...
+#'     174  | 0.5           | 0.5           | 2014 | POLYGON ((0 0, 0 1, 1 1, 1 ...
+#'     175  | 0.5           | 0.5           | 2023 | POLYGON ((0 0, 0 1, 1 1, 1 ...
 #' }
 #' @return The function return ggplot R plot.
 #' @export
-deploiement_density_dcp <- function(dataframe) {
+density_dcp_1deg <- function(dataframe) {
   # 0 - Global variables assignement ----
   year <- NULL
   count <- NULL
@@ -22,16 +22,10 @@ deploiement_density_dcp <- function(dataframe) {
   sf::st_crs(dataframe) <- 4326
   report_year <- max(dataframe$year)
   years <- c((report_year - 8):report_year)
-  ocean <- dataframe$ocean_code[1]
   # 2 - Graphic design ----
-  if (ocean == 1) {
-    ocean_xlim <- c(-40, 20)
-    ocean_ylim <- c(-30, 30)
-  } else if (ocean == 2) {
-    ocean_xlim <- c(30, 90)
-    ocean_ylim <- c(-30, 20)
-  }
-  func_plot_density_dep <- function(dt, yr) {
+  ocean_xlim <- c(-40, 20)
+  ocean_ylim <- c(-30, 30)
+  func_plot_density <- function(dt, yr) {
     data <- dt %>%
       dplyr::filter(year == !!yr)
 
@@ -43,35 +37,31 @@ deploiement_density_dcp <- function(dataframe) {
       ggplot2::ylab("") +
       ggplot2::xlab("") +
       ggplot2::theme_bw() +
-      ggplot2::scale_x_continuous(breaks = seq(-180,
-                                               180,
+      ggplot2::scale_x_continuous(breaks = seq(from = -180,
+                                               to = 180,
                                                by = 10)) +
-      ggplot2::scale_y_continuous(breaks = seq(-90,
-                                               90,
+      ggplot2::scale_y_continuous(breaks = seq(from = -90,
+                                               to = 90,
                                                by = 10)) +
       ggplot2::geom_sf(data = data,
                        ggplot2::aes(fill = count),
                        lwd = 0,
                        color = NA) +
-      ggplot2::scale_fill_gradient(low = "#CCFFFF",
-                                   high = "#000033") +
+      ggplot2::scale_fill_gradient(low = "cornsilk1",
+                                   high = "brown") +
       ggplot2::coord_sf(xlim = ocean_xlim,
                         ylim = ocean_ylim) +
-      ggplot2::theme(legend.position = c(0.17,
-                                         0.29),
-                     legend.key.size = ggplot2::unit(0.27,
-                                                     "cm"),
-                     legend.key.width = ggplot2::unit(.27,
-                                                      "cm"),
+      ggplot2::theme(legend.position = c(0.14, 0.3),
+                     legend.key.size = ggplot2::unit(0.3, "cm"),
+                     legend.key.width = ggplot2::unit(0.3, "cm"),
                      plot.title = ggplot2::element_text(hjust = 0.5,
                                                         color = "black",
-                                                        size = 8,
+                                                        size = 12,
                                                         face = "bold"),
-                     plot.subtitle = ggplot2::element_text(color = "blue"),
-                     plot.caption = ggplot2::element_text(color = "black"),
-                     legend.text = ggplot2::element_text(size = 6),
                      axis.text.x = ggplot2::element_text(size = 6),
                      axis.text.y = ggplot2::element_text(size = 6),
+                     plot.subtitle = ggplot2::element_text(color = "blue"),
+                     plot.caption = ggplot2::element_text(color = "black"),
                      plot.margin = ggplot2::unit(c(0.5, 0.5, 0.5, 0.5),
                                                  "lines")) +
       ggplot2::guides(fill = ggplot2::guide_colorbar(title = "Number",
@@ -80,10 +70,14 @@ deploiement_density_dcp <- function(dataframe) {
                                                                                          face = "bold",
                                                                                          colour = "black",
                                                                                          angle = 0))) +
-      ggplot2::ggtitle(yr)
+      ggplot2::ggtitle(year)
+
     return(p)
   }
-  plots <- lapply(years, function(yr) func_plot_density_dep(dataframe, yr))
+
+  # Creation of graphs for each year
+  plots <- lapply(years, function(yr) func_plot_density(dataframe, yr))
+  # Arrangement of graphics in a figure
   figure <- ggpubr::ggarrange(plotlist = plots,
                               labels = NULL,
                               widths = rep(1, length(plots)),
